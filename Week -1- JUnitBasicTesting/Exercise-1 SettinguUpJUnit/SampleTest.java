deb56bb8a01365ef642849a77a34017de6976f4c
dangetihemasree-web <dangetihemasree@gmail.com>
2026-07-12 00:39:41 +0530

    Create SampleTest.java

---
+++ Week -1- JUnitBasicTesting/Exercise-1 SettinguUpJUnit/SampleTest.java
@@ -0,0 +1,11 @@
+ackage org.example;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testExample() {
+        System.out.println("JUnit Test Executed Successfully!");
+    }
+}
