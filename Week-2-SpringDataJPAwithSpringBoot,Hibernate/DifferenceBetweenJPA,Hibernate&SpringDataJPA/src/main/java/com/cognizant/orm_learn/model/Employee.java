c24246b004c19095acbc3443a615031593a99569
dangetihemasree-web <dangetihemasree@gmail.com>
2026-07-12 22:42:02 +0530

    Add files via upload

---
+++ Week-2-SpringDataJPAwithSpringBoot,Hibernate/DifferenceBetweenJPA,Hibernate&SpringDataJPA/src/main/java/com/cognizant/orm_learn/model/Employee.java
@@ -0,0 +1,27 @@
+package com.cognizant.orm_learn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    // Default constructor (Required by JPA/Hibernate)
+    public Employee() {}
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
