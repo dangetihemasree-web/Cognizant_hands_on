b05419f2e1d164fb111a0e249681dd20a2e356bc
dangetihemasree-web <dangetihemasree@gmail.com>
2026-07-17 20:45:33 +0530

    Add files via upload

---
+++ Week-5-React/1. ReactJS-HOL/HandsOn-4-BlogApp/src/App.js
@@ -0,0 +1,11 @@
+import Posts from './Components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week-5-React/1. ReactJS-HOL/HandsOn-4-BlogApp/src/Components/Posts.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { posts: [] };
+    }
+
+    loadPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => this.setState({ posts: data }));
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert("An error occurred: " + error);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
